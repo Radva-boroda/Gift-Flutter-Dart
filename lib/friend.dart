a3debe7dcd1685459a2f6ba999605bee5da35979
RNV <RNV@d.c>
2023-07-14 06:54:53 +0300

    1.add friends

---
+++ lib/friend.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class Friend {
+
+  final int id ;
+  final String name;
+  final Color color;
+
+  Friend( this.id, this.color, this.name );
+
+}
--- lib/friends_page.dart
+++ lib/friends_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:santa/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'friend.dart';
+
 class FriendsPage extends StatefulWidget {
 
   static const routeName = "/friends";
@@ -16,6 +18,7 @@ class FriendsPage extends StatefulWidget {
 class _FriendsPageState extends State<FriendsPage>{
 
   String? _teamName;
+List _friends = [];
 
   @override
   void initState() {
@@ -41,21 +44,33 @@ class _FriendsPageState extends State<FriendsPage>{
         title: Text(_teamName?? ""),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _add,
-        tooltip: 'Increment',
-        child: const Icon(Icons.plus_one),
+        child: ListView.separated(
+          itemBuilder: _itemBilder,
+            separatorBuilder: _separatorBuilder,
+            itemCount: _friends.length,
+        )),
+        floatingActionButton: FloatingActionButton(
+      onPressed: _add,
+      tooltip: 'Increment',
+      child: const Icon(Icons.plus_one),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+Widget _itemBilder(BuildContext context, int index){
+    return Container(color: Colors.black, height: 100);
+
 }
 
-Future<void> _add() async {
+  Widget _separatorBuilder(BuildContext context, int index){
+return const SizedBox(height: 10);
+  }
 
+
+Future<void> _add() async {
+    final friend = Friend(_friends.length, Colors.grey, "My friend" );
+    setState(() {
+      _friends.add(friend);
+    });
+}
 }
